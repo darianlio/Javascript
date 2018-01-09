5f666ccc84c353cbbc7c1abc4a2394e85c63ee8c
Darian Lio <14dl50@queensu.ca>
2018-01-09 01:58:32 -0500

    learning java basics
    - primitives
    - variables

---
+++ jsBasics.js
@@ -0,0 +1,45 @@
+//5 Primitive Data Types in Javscript
+
+//Numbers
+4
+9.3
+-10
+
+//Strings
+"Hello World"
+"43"
+
+//Concatenation
+"charlie" + "brown" //charliebrown
+
+//Escape Characters start with "\"
+"Singing \"Do wah diddy, diddy, dum diddy do\" "
+"This is a backslash: \\"
+
+//Strings have a length property
+"hello world".length
+
+//Access individual characters using [] and an index
+"hello"[0] //"h"
+"hello"[4] //"o"
+
+//Booleans
+true
+false
+
+//null and undefined
+null
+undefined
+
+//Numbers
+4+5 //9
+1/5 //0.2
+
+//Modulo
+10 % 3 //1
+
+//Variables
+var name = "Rusty";
+var number = 73;
+var isAdorable = true;
+
