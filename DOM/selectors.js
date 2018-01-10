536d2179f42ec3bc46b2808ae579790acfe916ca
Darian Lio <14dl50@queensu.ca>
2018-01-09 20:07:29 -0500

    manipulating in JS

--- DOM/selectors.js
+++ DOM/selectors.js
@@ -10,3 +10,21 @@ console.log(tag[0]);
 //Query Selector CSS-Style
 tag = document.querySelector("#highlight"); //returns first element called #highlight
 tag = document.querySelectorAll("h1"); //returns all elements called h1
+
+//Manipulators
+//Styles
+tag.style.color ="blue"
+tag.style.border ="blue"
+
+//Toggles
+tag.classList.add("class");
+tag.classList.remove("class");
+tag.classList.toggle("class");
+
+//textContent
+tag.textContent //retrieve text content
+tag.textContent ="blah blah" //alter text content
+
+//innerHTML
+tag.innerHTML //returns string with all HTML content
+
