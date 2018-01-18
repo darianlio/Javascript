7a83cf6dea3b16b362588333e4e887baeefa2717
Darian Lio <14dl50@queensu.ca>
2018-01-17 21:59:46 -0500

    review fundamentals

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,11 +1,13 @@
-var name = 'John';
-console.log(name);
+//Variables
+//var name = 'John';
+//console.log(name);
+//
+//var lastName = 'Smith';
+//console.log(lastName);
+//
+//var age = 26;
+//console.log(age);
+//
+//var fullAge = true;
+//console.log(fullAge);
 
-var lastName = 'Smith';
-console.log(lastName);
-
-var age = 26;
-console.log(age);
-
-var fullAge = true;
-console.log(fullAge);
