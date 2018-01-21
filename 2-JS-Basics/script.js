0a10abde78ec8d3b402441199189384360d2912e
Darian Lio <14dl50@queensu.ca>
2018-01-20 23:26:32 -0500

    function review

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -35,67 +35,89 @@
 //alert(name + " is a " + age + " years old " + job + ". Is he married? " + isMarried);
 
 // Operators
-var now = 2018;
-var birthYear = now - 21;
+//var now = 2018;
+//var birthYear = now - 21;
+//
+//birthYear = now - 21 * 2;
+//
+//console.log(birthYear);
+//
+//var ageJohn = 30;
+//var ageMark = 30;
+//
+//ageJohn = ageMark = (3 + 5) * 4 - 6;
+//
+//ageJohn++;
+//ageMark *= 2;
+//
+//console.log(ageJohn);
+//console.log(ageMark);
+//
+//// If/else statments
+//var name = "Darian";
+//var age = 20;
+//var isMarried = "no";
+//
+//if (isMarried === "yes"){
+//    console.log(name + " is Married");
+//} else {
+//    console.log(name + " will hopefully marry soon");
+//}
+//
+//isMarried = false;
+//if(isMarried){
+//    console.log(name + " is Married");
+//} else {
+//    console.log(name + " will hopefully marry soon");
+//}
+//
+////Boolean logic and switch
+//var age = 25;
+//if (age < 20){
+//    console.log("John is a teenager");
+//} else if (age > 20 && age < 30){
+//    console.log("John is a young man");
+//}
+//else {
+//    console.log("John is a man");
+//}
+//
+//var job = "teacher";
+//
+//job = prompt("What does John do?");
+//
+//switch(job){
+//    case "teacher":
+//        console.log("John is a teacher");
+//        break;
+//    case "driver":
+//        console.log("John is a driver");
+//        break;
+//    case "cop":
+//        console.log("John helps fight crime");
+//        break;
+//    default: 
+//        console.log("JOhn does something else.");
+//}
 
-birthYear = now - 21 * 2;
+//Function
+function calculateAge(yearOfBirth){
+    var age = 2016 - yearOfBirth;
+    return age;
+}
 
-console.log(birthYear);
-
-var ageJohn = 30;
-var ageMark = 30;
-
-ageJohn = ageMark = (3 + 5) * 4 - 6;
-
-ageJohn++;
-ageMark *= 2;
-
+var ageJohn = calculateAge(1990);
 console.log(ageJohn);
-console.log(ageMark);
 
-// If/else statments
-var name = "Darian";
-var age = 20;
-var isMarried = "no";
-
-if (isMarried === "yes"){
-    console.log(name + " is Married");
-} else {
-    console.log(name + " will hopefully marry soon");
+function yearsUntilRetirement(name, yearofBirth){
+    var age = calculateAge(yearOfBirth);
+    var retirement = 65 - age;
+    
+    if (retirement >= 0){
+        console.log(retirement);   
+    } else {
+        console.log("Is already retired.");
+    }
 }
 
-isMarried = false;
-if(isMarried){
-    console.log(name + " is Married");
-} else {
-    console.log(name + " will hopefully marry soon");
-}
-
-//Boolean logic and switch
-var age = 25;
-if (age < 20){
-    console.log("John is a teenager");
-} else if (age > 20 && age < 30){
-    console.log("John is a young man");
-}
-else {
-    console.log("John is a man");
-}
-
-var job = "teacher";
-
-job = prompt("What does John do?");
-
-switch(job){
-    case "teacher":
-        console.log("John is a teacher");
-        break;
-    case "driver":
-        console.log("John is a driver");
-        break;
-    case "cop":
-        console.log("John helps fight crime");
-        break;
-    default: 
-        console.log("JOhn does something else.");
-}
+yearsUntilRetirement("John", 1990);
