396ae73ad17fce2538aa04d67e7afde3dcf4c44c
Darian Lio <14dl50@queensu.ca>
2018-01-21 16:54:48 -0500

    objects and methods review

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -153,32 +153,79 @@
 //    console.log("John is not a teacher")
 //}
 
-//Objects
+////Objects
+//var john = {
+//    name: "John",
+//    lastName: "Smith",
+//    yearOfBirth: 1990,
+//    job: "teacher",
+//    isMarried: "false"
+//};
+//
+//console.log(john);
+//console.log(john.lastName);
+//console.log(john["lastName"]);
+//
+//var xyz = "job";
+//console.log(john[xyz]);
+//
+//john.lastName = "Miller";
+//john["job"] = "programmer";
+//
+//console.log(john);
+//
+//var jane = new Object();
+//jane.name = "Jane";
+//jane.lastName = "Smith";
+//jane["yearOfBirth"] = 1969;
+//jane["job"] = "retired";
+//jane["isMarried"] = true;
+//
+//console.log(jane);
+
+//Objects and Methods
+
+//V1
+//var john = {
+//    name: "John",
+//    lastName: "Smith",
+//    yearOfBirth: 1990,
+//    job: "teacher",
+//    isMarried: false,
+//    family: ["Jane", "Mark", "Bob"],
+//    calculateAge: function(){
+//        return 2016 - this.yearOfBirth;
+//    }
+//};
+//
+//console.log(john);
+//console.log(john.family[2]);
+//console.log(john.calculateAge());
+//var age = john.calculateAge();
+//john.age = age;
+
+//V2
 var john = {
     name: "John",
     lastName: "Smith",
     yearOfBirth: 1990,
     job: "teacher",
-    isMarried: "false"
+    isMarried: false,
+    family: ["Jane", "Mark", "Bob"],
+    calculateAge: function(){
+        this.age = 2016 - this.yearOfBirth;
+    }
 };
 
-console.log(john);
-console.log(john.lastName);
-console.log(john["lastName"]);
-
-var xyz = "job";
-console.log(john[xyz]);
-
-john.lastName = "Miller";
-john["job"] = "programmer";
-
+jon.calculateAge();
 console.log(john);
 
-var jane = new Object();
-jane.name = "Jane";
-jane.lastName = "Smith";
-jane["yearOfBirth"] = 1969;
-jane["job"] = "retired";
-jane["isMarried"] = true;
+var mike = {
+    yearOfBirth: 1950,
+    calculateAge: function(){
+        this.age = 2016 - this.yearOfBirth;
+    }
+};
 
-console.log(jane);
+mike.calculateAge();
+console.log(mike);
