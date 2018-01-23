c52c2e7acd76004d92ffca7326068d6379626cb2
Darian Lio <14dl50@queensu.ca>
2018-01-23 18:02:35 -0500

    working with dom manipulation

--- 4-DOM-Pig-Game-Final/app.js
+++ 4-DOM-Pig-Game-Final/app.js
@@ -17,3 +17,7 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector("#current-" + activePlayer).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+
+document.querySelector(".dice").style.display = "none";
